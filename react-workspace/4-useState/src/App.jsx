8d29ad0c4a77db55df014780753c2f3e09500215
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2026-01-14 17:52:59 +0300

    Update App.jsx
    useState

--- react-workspace/4-useState/src/App.jsx
+++ react-workspace/4-useState/src/App.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 
 function App() {
-  const [firstName] = useState("Ali Kaan");
-  const [lastName] = useState("Koç");
+  const [count, setCount] = useState(0);
 
+  const arttir = () => {
+    setCount(count + 1);
+  };
+  const azalt = () => {
+    if (count >= 1) setCount(count - 1);
+  };
   return (
     <div>
-      <div>{firstName}</div>
-      <div>{lastName}</div>
+      <div>
+        <p>{count}</p>
+        <button onClick={arttir}>Arttır</button>
+        <button onClick={azalt}>Azalt</button>
+      </div>
+      <div></div>
     </div>
   );
 }
+
 export default App;
