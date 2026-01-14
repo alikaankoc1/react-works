1729c6a93df8c7f812df118ca958e518590b728e
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2026-01-14 17:13:41 +0300

    update

---
+++ react-workspace/components-yapisi/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <hr />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-workspace/components-yapisi/src/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <p>Kullanıcı Adı:</p>
+        <input type="text" />
+      </div>
+      <div>
+        <p>Şifreniz:</p>
+        <input type="password" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
