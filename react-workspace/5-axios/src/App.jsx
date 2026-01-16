5b29a929b095e2d9ff8af55f5660bfc4639bcdac
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2026-01-16 19:26:52 +0300

    Add PUT operation
    Added functionality for updating existing data using a PUT request. Implemented the necessary function and endpoint for updating records. Users can now update a specific entry. The code includes the axios PUT function and an example usage.

--- react-workspace/5-axios/src/App.jsx
+++ react-workspace/5-axios/src/App.jsx
@@ -3,14 +3,25 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:3005";
 function App() {
+  // get almak
   const getAllUsers = async () => {
     const response = await axios.get(BASE_URL + "/users");
     console.log(response);
   };
 
+  // post göndermek
   const createUser = async (newUser) => {
     const response = await axios.post(`${BASE_URL}/users`, newUser);
     console.log("response", response.data);
+  };
+
+  //put güncelleme
+  const updateUser = async (userId, updatedUser) => {
+    const response = await axios.put(
+      `${BASE_URL}/users/${userId}`,
+      updatedUser
+    );
+    console.log("new data ", response.data);
   };
 
   useEffect(() => {
@@ -21,6 +32,12 @@ function App() {
       class: "18",
     };
     createUser(newUser);
+
+    updateUser("1690", {
+      username: "La Liga",
+      password: "wolwes",
+      class: "98",
+    });
   }, []);
 
   return <div></div>;
